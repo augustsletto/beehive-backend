af3aa481e87f99211ac7e15dce0b812176430e87
august sletto <augustsletto@gmail.com>
2024-03-21 15:55:49 +0000

    added django allauth for registration

--- beehive_backend/settings.py
+++ beehive_backend/settings.py
@@ -24,7 +24,22 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
 
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'beehive_backend.serializers.CurrentUserSerializer'
+}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -36,7 +51,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-augustslett-beehiveback-9vq0t41vg9s.ws-eu110.gitpod.io']
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-augustsletto-djangorest-g4aditusjc6.ws-eu110.gitpod.io',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,6 +67,13 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
     'profiles',
     'posts',
@@ -57,6 +81,8 @@ INSTALLED_APPS = [
     'likes',
     'followers',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
