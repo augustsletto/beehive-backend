2ef7ad75f3594219ed75c3d78edd0f804099a391
august sletto <augustsletto@gmail.com>
2024-03-22 18:17:21 +0000

    test

--- beehive_backend/settings.py
+++ beehive_backend/settings.py
@@ -56,14 +56,14 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
     'localhost',
+    'https://beehive-social-3ec964865be0.herokuapp.com'
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
