afdd44d070d0d34dd23fafd4811bd8f3d78a3b88
august sletto <augustsletto@gmail.com>
2024-03-27 22:58:10 +0000

    test

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 
-    path('products/', views.getProducts, name="products"),
-    path('products/<str:pk>/', views.getProduct, name="product"),
+    path('products/', views.getProducts.as_view(), name="products"),
+    path('products/<str:pk>/', views.getProduct.as_view(), name="product"),
 ]
