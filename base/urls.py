c78884243c540da8215bb402fb165e61febe5e9b
august sletto <augustsletto@gmail.com>
2024-03-27 23:06:47 +0000

    git commit -m testing test

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('products/', views.getProducts, name="products"),
-    path('products/<str:pk>/', views.getProduct, name="product"),
+    path('products/', views.getProducts.as_view(), name="products"),
+    path('products/<str:pk>/', views.getProduct.as_view(), name="product"),
 ]
