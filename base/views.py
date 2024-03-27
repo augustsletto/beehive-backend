bb8129f40376ecc6a66b1940dd3185095dab4e3d
august sletto <augustsletto@gmail.com>
2024-03-27 14:17:27 +0000

    Fixed errors

--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .models import Product
 
 from .products import products
+from .serializers import ProductSerializer
 
 
 @api_view(['GET'])
@@ -31,14 +32,13 @@ def getRoutes(request):
     return Response(routes)
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-    return Response(product)
+    return Response(serializer.data)
